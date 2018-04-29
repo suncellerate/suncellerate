c7f785164d5a249f1c853a065903e3c2280f19e9
Andrew Koines <andrew@andrewkoines.com>
2018-04-28 21:54:13 -0700

    Loads Bootstrap JS, adds Navbar

--- app/javascript/components/NavBar.jsx
+++ app/javascript/components/NavBar.jsx
@@ -3,22 +3,30 @@ import React from 'react';
 class NavBar extends React.Component {
   render () {
     return (
-      <nav>
-        <div className="navWide">
-            <div className="wideDiv">
-                <a href="#">Link 1</a>
-                <a href="#">Link 2</a>
-                <a href="#">Link 3</a>
-              </div>
-          </div>
-          <div className="navNarrow">
-            <i className="fa fa-bars fa-2x"></i>
-              <div className="narrowLinks">
-                  <a href="#">Link 1</a>
-                  <a href="#">Link 2</a>
-                  <a href="#">Link 3</a>
-              </div>
+      <nav className={`navbar navbar-expand-lg navbar-light bg-light`}>
+        <a className={`navbar-brand`} href="#">Navbar</a>
+        <button className={`navbar-toggler`} type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className={`navbar-toggler-icon`}></span>
+        </button>
+
+        <div className={`collapse navbar-collapse`} id="navbarSupportedContent">
+          <ul className={`navbar-nav mr-auto`}>
+            <li className={`nav-item active`}>
+              <a className={`nav-link`} href="#">Home <span className={`sr-only`}>(current)</span></a>
+            </li>
+            <li className={`nav-item`}>
+              <a className={`nav-link`} href="#">Link</a>
+            </li>
+            <li className={`nav-item`}>
+              <a className={`nav-link`} href="#">Link</a>
+            </li>
+            <li className={`nav-item`}>
+              <a className={`nav-link`} href="#">Link</a>
+            </li>
+
+          </ul>
         </div>
+
       </nav>
     );
   }
