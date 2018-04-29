3c4771836c823111db92ee3255147577ce8bea83
Andrew Koines <andrew@andrewkoines.com>
2018-04-29 15:27:57 -0700

    WIP - Styling search results

--- app/javascript/components/Search.jsx
+++ app/javascript/components/Search.jsx
@@ -26,7 +26,7 @@ class Search extends React.Component {
   }
   render() {
     const searchView = this.state.view === 'search' ?
-        <div className={`row`}>
+        <div className={`container`}>
           <SearchResults listings={sampleData} handleClick={this.handleClick.bind(this)}/>
         </div> : null
     const detailedView = this.state.view ==='detailedView' ? <DetailedView listing={this.state.clickedListing}/> : null
