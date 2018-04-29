44c1835039fb88aa51a6b56b9fb3a85e32e85bb2
Andrew Koines <andrew@andrewkoines.com>
2018-04-29 16:36:56 -0700

    uses bootstrap columns on InvestorPage

--- app/javascript/components/InvestorPage.jsx
+++ app/javascript/components/InvestorPage.jsx
@@ -21,17 +21,19 @@ export default class InvestorPage extends React.Component {
 
     return (
       <div>
-        <div  className={`greeting`}>
+        <div className={`greeting col-12`}>
           <h1> Thinking of Investing? </h1>
           <h2> Tell us where: </h2>
         </div>
-        <form>
-          <h3> Zip Code: </h3>
-          <input type="text" name="zip"/><br/>
-          <h3> Email: </h3>
-          <input type="text" name="email"/><br/>
-          <input type="submit" className={`btn btn-primary`} id="find-projects" value="Find Projects!" onClick={this.handleClick.bind(this)}/>
-        </form>
+        <div className={`col-12`}>
+          <form>
+            <h3> Zip Code: </h3>
+            <input type="text" name="zip"/><br/>
+            <h3> Email: </h3>
+            <input type="text" name="email"/><br/>
+            <input type="submit" className={`btn btn-primary`} id="find-projects" value="Find Projects!" onClick={this.handleClick.bind(this)}/>
+          </form>
+        </div>
       {searchResults}
       </div>
     );
