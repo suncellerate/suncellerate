13478a84f321f4adaabb59304cbddce5adf97fed
Andrew Koines <andrew@andrewkoines.com>
2018-04-29 23:39:01 -0700

    Uses list to allow multiple element return, giving the desired structure

--- app/javascript/components/AuthLinks.jsx
+++ app/javascript/components/AuthLinks.jsx
@@ -25,12 +25,10 @@ class AuthLinks extends React.Component {
   render () {
     if (this.state.signedIn == 'false'){
       return (
-        //This gives us 2 ULs, but for some reason react crashes if given two anchor
-        //tags here not wrapped in some other tag e.g. ul, div, or span.
-        <ul className={`navbar-nav ml-auto`}>
-        <li><a className={`nav-link`} href="/users/sign_up">Sign Up</a></li>
+        [
+        <li><a className={`nav-link`} href="/users/sign_up">Sign Up</a></li>,
         <li><a className={`nav-link`} href="/users/sign_in">Sign In</a></li>
-        </ul>
+        ]
       );
     }
     else {
